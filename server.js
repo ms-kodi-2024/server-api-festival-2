3f675d99652180f7fea7d8ef0aa813ae9a057028
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-03-27 07:23:24 +0100

    Add db in server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
+
 const app = express();
 
 app.use(cors());
