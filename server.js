a4a33ffbce63b252b3897ca9e0a0f4b0adc85dff
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-04-11 07:01:42 +0200

    Remove support for websocket from server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const socketIo = require('socket.io');
 const mongoose = require('mongoose');
+const pusher = require('./pusher');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -27,23 +27,6 @@ db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
-});
-const io = socketIo(server);
-
-io.on('connection', async (socket) => {
-  console.log('New socket!');
-  const Seat = require('./models/seat.model');
-  try {
-    const seats = await Seat.find();
-    socket.emit('seatsUpdated', seats);
-  } catch (error) {
-    console.log('Error retrieving seats: ' + error.message);
-  }
-});
-
-app.use((req, res, next) => {
-  req.io = io;
-  next();
 });
 
 app.use('/api/testimonials', testimonialsRoutes);
